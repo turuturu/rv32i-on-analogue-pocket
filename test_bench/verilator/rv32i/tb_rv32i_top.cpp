c33ce0c99c8ba364d657970b941a9234dbfee55c
turuturu <t.tsurumi@gmail.com>
2024-01-28 21:04:12 +0900

    Add trace functionality and load binary file to ROM

--- test_bench/verilator/rv32i/tb_rv32i_top.cpp
+++ test_bench/verilator/rv32i/tb_rv32i_top.cpp
@@ -1,11 +1,16 @@
 #include <gtest/gtest.h>
+#include <stdio.h>
 #include <verilated.h>
+#include <verilated_vcd_c.h>
 #include <climits>
 #include <random>
 #include <iostream>
 #include <fstream>
+#include <string>
+
 #include "Vrv32i_rv32i_top.h"
 #include "Vrv32i_rv32i_top_rom.h"
+#include "Vrv32i_rv32i_top_registers.h"
 #include "Vrv32i_rv32i_top_rv32i_top.h"
 
 
@@ -21,7 +26,7 @@ class Rv32iTopTest : public ::testing::Test {
         delete dut;
     }
 
-    void load_binary_to_rom(std::string filename){
+    int load_binary_to_rom(std::string filename){
         std::ifstream ifs(BASE_PATH + filename, std::ios::binary);
         ifs.seekg(0, std::ios::end);
         long long int size = ifs.tellg();
@@ -29,7 +34,7 @@ class Rv32iTopTest : public ::testing::Test {
 
         if (size < 0) {
           std::cout << "Not find file: " << filename << std::endl;
-          return;
+          return -1;
         }
         char* data = new char[size];
         ifs.read(data, size);
@@ -37,15 +42,41 @@ class Rv32iTopTest : public ::testing::Test {
             dut->rv32i_top->rom0->inner_rom[i/4] = data[i] | data[i+1] << 8 | data[i+2] << 16 | data[i+3] << 24;
         }
         delete data;
+        return size / 4;
     }
 };
 
 namespace {
 
 TEST_F(Rv32iTopTest, rv32ui_p_add) {
-    load_binary_to_rom("rv32ui-p-add.bin");
-    int instr = 0b0000'0000'0000'0000'0000'0000'0011'0111;
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    Verilated::traceEverOn(true);
+    dut->trace(tfp, 1000);
+    tfp->open("./rv32ui_p_add.vcd");
+
+    int cnt = 0;
+    int len = load_binary_to_rom("rv32ui-p-add.bin");
+    dut->clk = 0;
     dut->eval();
+    tfp->dump(cnt++);
+    dut->reset_n = 0;
+    dut->clk = 1;
+    dut->eval();
+    tfp->dump(cnt++);
+
+    dut->reset_n = 1;
+    for(int i = 0; i < len; i++){
+        dut->clk = 0;
+        dut->eval();
+        tfp->dump(cnt++);
+        dut->clk = 1;
+        dut->eval();
+        tfp->dump(cnt++);
+      }
+      dut->final();
+      tfp->close();
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
   //  ASSERT_EQ(dut->alu_op, Valu_rv32i::alu_op_e::ALU_ADD);
 } 
 
