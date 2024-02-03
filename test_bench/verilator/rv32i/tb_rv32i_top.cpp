c5847c3e4c4a93b10b05daca3f55b91f5b59954e
turuturu <t.tsurumi@gmail.com>
2024-02-03 15:27:16 +0900

    Refactor test case to use a function and add assertions

--- test_bench/verilator/rv32i/tb_rv32i_top.cpp
+++ test_bench/verilator/rv32i/tb_rv32i_top.cpp
@@ -44,40 +44,47 @@ class Rv32iTopTest : public ::testing::Test {
         delete data;
         return size / 4;
     }
+
+    void run_test(std::string filename){
+        Verilated::traceEverOn(true);
+        VerilatedVcdC* tfp = new VerilatedVcdC;
+        Verilated::traceEverOn(true);
+        std::string vcd_name = "vcd/" + filename + ".vcd";
+        std::string bin_name = filename + ".bin";
+        dut->trace(tfp, 1000);
+        tfp->open(vcd_name.c_str());
+
+        int len = load_binary_to_rom(bin_name.c_str());
+        int cnt = 0;
+        dut->clk = 0;
+        dut->reset_n = 0;
+        dut->eval();
+        dut->clk = 1;
+        dut->eval();
+        dut->clk = 0;
+        dut->eval();
+        tfp->dump(cnt++);
+        for(int i = 0; i < len; i++){
+            dut->clk = 1;
+            dut->eval();
+            tfp->dump(cnt++);
+        dut->reset_n = 1;
+            dut->clk = 0;
+            dut->eval();
+            tfp->dump(cnt++);
+        }
+        dut->final();
+        tfp->close();
+        ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+    }
+
 };
 
 namespace {
 
 TEST_F(Rv32iTopTest, rv32ui_p_add) {
-    Verilated::traceEverOn(true);
-    VerilatedVcdC* tfp = new VerilatedVcdC;
-    Verilated::traceEverOn(true);
-    dut->trace(tfp, 1000);
-    tfp->open("./rv32ui_p_add.vcd");
-
-    int cnt = 0;
-    int len = load_binary_to_rom("rv32ui-p-add.bin");
-    dut->clk = 0;
-    dut->eval();
-    tfp->dump(cnt++);
-    dut->reset_n = 0;
-    dut->clk = 1;
-    dut->eval();
-    tfp->dump(cnt++);
-
-    dut->reset_n = 1;
-    for(int i = 0; i < len; i++){
-        dut->clk = 0;
-        dut->eval();
-        tfp->dump(cnt++);
-        dut->clk = 1;
-        dut->eval();
-        tfp->dump(cnt++);
-      }
-      dut->final();
-      tfp->close();
+    run_test("rv32ui-p-add");
     ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
-  //  ASSERT_EQ(dut->alu_op, Valu_rv32i::alu_op_e::ALU_ADD);
 } 
 
 }  // namespace
