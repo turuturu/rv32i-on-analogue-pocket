fe4cf39a80a1a92bff224afdaa46e990b3cd8af9
turuturu <t.tsurumi@gmail.com>
2024-02-24 18:57:52 +0900

    Remove commented-out code and add RV32UI-P test cases

--- test_bench/verilator/rv32i/tb_rv32i_top.cpp
+++ test_bench/verilator/rv32i/tb_rv32i_top.cpp
@@ -46,9 +46,9 @@ class Rv32iTopTest : public ::testing::Test {
             }
             i += 4;
         }
-        for(int j = 0; j < i/4; j++){
-            std::cout << std::setfill('0') << std::setw(8) << std::hex << dut->rv32i_top->rom0->inner_rom[j] << std::endl;
-        }
+        // for(int j = 0; j < i/4; j++){
+        //     std::cout << std::setfill('0') << std::setw(8) << std::hex << dut->rv32i_top->rom0->inner_rom[j] << std::endl;
+        // }
 
         return i / 4;
     }
@@ -63,9 +63,6 @@ class Rv32iTopTest : public ::testing::Test {
         tfp->open(vcd_name.c_str());
 
         int len = load_binary_to_rom(bin_name.c_str());
-        ASSERT_EQ(dut->rv32i_top->rom0->inner_rom[0], 0x0500006f);
-        ASSERT_EQ(dut->rv32i_top->rom0->inner_rom[1], 0x34202f73);
-//        ASSERT_EQ(dut->rv32i_top->rom0->inner_rom[80], 0x00000093);
         int cnt = 0;
         dut->clk = 0;
         dut->reset_n = 0;
@@ -96,7 +93,176 @@ namespace {
 TEST_F(Rv32iTopTest, rv32ui_p_add) {
     run_test("rv32ui-p-add");
     ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
-} 
+}
+TEST_F(Rv32iTopTest, rv32ui_p_addi) {
+    run_test("rv32ui-p-addi");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_and) {
+    run_test("rv32ui-p-and");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_andi) {
+    run_test("rv32ui-p-andi");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_auipc) {
+    run_test("rv32ui-p-auipc");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_beq) {
+    run_test("rv32ui-p-beq");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_bge) {
+    run_test("rv32ui-p-bge");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_bgeu) {
+    run_test("rv32ui-p-bgeu");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_blt) {
+    run_test("rv32ui-p-blt");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_bltu) {
+    run_test("rv32ui-p-bltu");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_bne) {
+    run_test("rv32ui-p-bne");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+// TEST_F(Rv32iTopTest, rv32ui_p_fence_i) {
+//     run_test("rv32ui-p-fence_i");
+//     ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+// }
+TEST_F(Rv32iTopTest, rv32ui_p_jal) {
+    run_test("rv32ui-p-jal");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_jalr) {
+    run_test("rv32ui-p-jalr");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_lb) {
+    run_test("rv32ui-p-lb");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_lbu) {
+    run_test("rv32ui-p-lbu");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_lh) {
+    run_test("rv32ui-p-lh");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_lhu) {
+    run_test("rv32ui-p-lhu");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_lui) {
+    run_test("rv32ui-p-lui");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_lw) {
+    run_test("rv32ui-p-lw");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_ma_data) {
+    run_test("rv32ui-p-ma_data");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_or) {
+    run_test("rv32ui-p-or");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_ori) {
+    run_test("rv32ui-p-ori");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_sb) {
+    run_test("rv32ui-p-sb");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_sh) {
+    run_test("rv32ui-p-sh");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_simple) {
+    run_test("rv32ui-p-simple");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_sll) {
+    run_test("rv32ui-p-sll");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_slli) {
+    run_test("rv32ui-p-slli");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_slt) {
+    run_test("rv32ui-p-slt");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_slti) {
+    run_test("rv32ui-p-slti");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_sltiu) {
+    run_test("rv32ui-p-sltiu");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_sltu) {
+    run_test("rv32ui-p-sltu");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_sra) {
+    run_test("rv32ui-p-sra");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_srai) {
+    run_test("rv32ui-p-srai");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_srl) {
+    run_test("rv32ui-p-srl");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_srli) {
+    run_test("rv32ui-p-srli");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_sub) {
+    run_test("rv32ui-p-sub");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_sw) {
+    run_test("rv32ui-p-sw");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+
+TEST_F(Rv32iTopTest, rv32ui_p_xor) {
+    run_test("rv32ui-p-xor");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
+TEST_F(Rv32iTopTest, rv32ui_p_xori) {
+    run_test("rv32ui-p-xori");
+    ASSERT_EQ(dut->rv32i_top->registers0->regs[3], 1);
+}
 
 }  // namespace
 
