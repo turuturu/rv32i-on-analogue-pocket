acb06dbdd7e5985f675e8f6f542bb7f4e7733c05
turuturu <t.tsurumi@gmail.com>
2024-07-20 20:54:26 +0900

    bug fix

--- src/rtl/rv32i/ram.sv
+++ src/rtl/rv32i/ram.sv
@@ -26,10 +26,20 @@ module ram import rv32i::*;
   logic [ADDR_LENGTH-1-2:0] addr1;
   logic [ADDR_LENGTH-1-2:0] addr2;
   logic [ADDR_LENGTH-1-2:0] addr3;
-  assign addr0 = addr[ADDR_LENGTH-1-2:0];
-  assign addr1 = addr0 + 1;
-  assign addr2 = addr0 + 2;
-  assign addr3 = addr0 + 3;
+  logic [ADDR_LENGTH-1-2:0] waddr0;
+  logic [ADDR_LENGTH-1-2:0] waddr1;
+  logic [ADDR_LENGTH-1-2:0] waddr2;
+  logic [ADDR_LENGTH-1-2:0] waddr3;
+  logic [1:0] offset;
+  assign offset = addr[1:0];
+  assign addr0 = addr[ADDR_LENGTH-1:2] + (offset < 1 ? 0 : 1);
+  assign addr1 = addr[ADDR_LENGTH-1:2] + (offset < 2 ? 0 : 1);
+  assign addr2 = addr[ADDR_LENGTH-1:2] + (offset < 3 ? 0 : 1);
+  assign addr3 = addr[ADDR_LENGTH-1:2];
+  assign waddr0 = addr[ADDR_LENGTH-1:2];
+  assign waddr1 = addr[ADDR_LENGTH-1:2] + (offset < 3 ? 0 : 1);
+  assign waddr2 = addr[ADDR_LENGTH-1:2] + (offset < 2 ? 0 : 1);
+  assign waddr3 = addr[ADDR_LENGTH-1:2] + (offset < 1 ? 0 : 1);
   logic [7:0] wdata0;
   logic [7:0] wdata1;
   logic [7:0] wdata2;
@@ -46,39 +56,104 @@ module ram import rv32i::*;
   assign odata1 = inner_ram1[addr1];
   assign odata2 = inner_ram2[addr2];
   assign odata3 = inner_ram3[addr3];
-  
-     
-  // logic [7:0] inner_ram [0:255]/*verilator public*/; // 1KB
-  // logic [ADDR_LENGTH-1:0] inner_addr/*verilator public*/;
   always_ff @(posedge clk) begin
     if (mem_op == MEM_STORE) begin
       case (ram_mask)
       RAM_MASK_B: begin
-        inner_ram0[addr0] <= wdata0;
+        case(offset)
+          0: inner_ram0[waddr0] <= wdata0;
+          1: inner_ram1[waddr0] <= wdata0;
+          2: inner_ram2[waddr0] <= wdata0;
+          3: inner_ram3[waddr0] <= wdata0;
+       endcase
       end
       RAM_MASK_H: begin
-        inner_ram0[addr0] <= wdata0;
-        inner_ram1[addr1] <= wdata1;
+        case(offset)
+          0: begin
+            inner_ram0[waddr0] <= wdata0;
+            inner_ram1[waddr1] <= wdata1;
+          end
+          1: begin
+            inner_ram1[waddr0] <= wdata0;
+            inner_ram2[waddr1] <= wdata1;
+          end
+          2: begin
+            inner_ram2[waddr0] <= wdata0;
+            inner_ram3[waddr1] <= wdata1;
+          end
+          3: begin
+            inner_ram3[waddr0] <= wdata0;
+            inner_ram0[waddr1] <= wdata1;
+          end
+       endcase
       end
       RAM_MASK_W: begin
-        inner_ram0[addr0] <= wdata0;
-        inner_ram1[addr1] <= wdata1;
-        inner_ram2[addr2] <= wdata2;
-        inner_ram3[addr3] <= wdata3;
+        case(offset)
+          0: begin
+            inner_ram0[waddr0] <= wdata0;
+            inner_ram1[waddr1] <= wdata1;
+            inner_ram2[waddr2] <= wdata2;
+            inner_ram3[waddr3] <= wdata3;
+          end
+          1: begin
+            inner_ram1[waddr0] <= wdata0;
+            inner_ram2[waddr1] <= wdata1;
+            inner_ram3[waddr2] <= wdata2;
+            inner_ram0[waddr3] <= wdata3;
+          end
+          2: begin
+            inner_ram2[waddr0] <= wdata0;
+            inner_ram3[waddr1] <= wdata1;
+            inner_ram0[waddr2] <= wdata2;
+            inner_ram1[waddr3] <= wdata3;
+          end
+          3: begin
+            inner_ram3[waddr0] <= wdata0;
+            inner_ram0[waddr1] <= wdata1;
+            inner_ram1[waddr2] <= wdata2;
+            inner_ram2[waddr3] <= wdata3;
+          end
+       endcase
       end
       default: begin
-        inner_ram0[addr0] <= wdata0;
-        inner_ram1[addr1] <= wdata1;
-        inner_ram2[addr2] <= wdata2;
-        inner_ram3[addr3] <= wdata3;
+        case(offset)
+          0: begin
+            inner_ram0[waddr0] <= wdata0;
+            inner_ram1[waddr1] <= wdata1;
+            inner_ram2[waddr2] <= wdata2;
+            inner_ram3[waddr3] <= wdata3;
+          end
+          1: begin
+            inner_ram1[waddr0] <= wdata0;
+            inner_ram2[waddr1] <= wdata1;
+            inner_ram3[waddr2] <= wdata2;
+            inner_ram0[waddr3] <= wdata3;
+          end
+          2: begin
+            inner_ram2[waddr0] <= wdata0;
+            inner_ram3[waddr1] <= wdata1;
+            inner_ram0[waddr2] <= wdata2;
+            inner_ram1[waddr3] <= wdata3;
+          end
+          3: begin
+            inner_ram3[waddr0] <= wdata0;
+            inner_ram0[waddr1] <= wdata1;
+            inner_ram1[waddr2] <= wdata2;
+            inner_ram2[waddr3] <= wdata3;
+          end
+       endcase
       end
       endcase
     end
   end
-  // assign inner_addr = addr[ADDR_LENGTH-1:0];
-  assign rdata = ram_mask == RAM_MASK_B ? {24'b0, odata0} : 
-                 ram_mask == RAM_MASK_H ? {16'b0, odata1,odata0} : 
-                 {odata3, odata2, odata1, odata0}; //ram_mask == RAM_MASK_W
+  logic [31:0] sorted_data;
+  assign sorted_data = offset == 0 ? {odata3, odata2, odata1, odata0} : 
+                        offset == 1 ? {odata0, odata3, odata2, odata1} : 
+                        offset == 2 ? {odata1, odata0, odata3, odata2} : 
+                        {odata2, odata1, odata0, odata3};
+  assign rdata = ram_mask == RAM_MASK_B ? {24'b0, sorted_data[7:0]} : 
+                 ram_mask == RAM_MASK_H ? {16'b0, sorted_data[15:0]} : 
+                 sorted_data; //ram_mask == RAM_MASK_W
 endmodule
 
 `endif
