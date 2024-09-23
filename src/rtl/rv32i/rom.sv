3fef6fa59326e77e10379e5d3dac52cd03aae49d
turuturu <t.tsurumi@gmail.com>
2024-09-23 10:23:16 +0900

    Update rom module with delay functionality

--- src/rtl/rv32i/rom.sv
+++ src/rtl/rv32i/rom.sv
@@ -6,20 +6,23 @@
 module rom import rv32i::*;
 #(
   parameter ADDR_LENGTH = 10, // 4KB
+  parameter DELAY = 0,
   // parameter ADDR_LENGTH = 8, // 1KB
   parameter MEM_SIZE = 2**ADDR_LENGTH
 )
 (
     input logic clk,
     input logic [31:0] addr,
-    output logic [31:0] data
+    input logic re,
+    output logic [31:0] data,
+    output logic oe
 );
 
   // logic [31:0] inner_rom [0:MEM_SIZE-1]/*verilator public*/; // 16KB
   (* ramstyle = "MLAB" *)logic [31:0] inner_rom [0:MEM_SIZE-1]/*verilator public*/;
   // logic [31:0] inner_rom [0:255]/*verilator public*/; // 1KB
   logic [ADDR_LENGTH-1:0] inner_addr;
-
+  logic [2:0] counter = 0;
   // // for test
   // initial begin
   //   $readmemh ("rom.txt", rom);
@@ -27,7 +30,12 @@ module rom import rv32i::*;
   // end
 
   always_ff @(posedge clk) begin
-    inner_addr <= addr[ADDR_LENGTH+1:2];
+    inner_addr <= (counter == 0 & re) ? addr[ADDR_LENGTH+1:2] : inner_addr;
+    counter <= DELAY == 0 ? 0 : 
+              (counter == 0 & re) ? 0 :
+              (counter == 0 | counter == DELAY) ? 0 :
+              counter + 1;
+    oe <= counter == DELAY;
   end
   assign data = inner_rom[inner_addr];
 endmodule
